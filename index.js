de1fd27b46d28c3cc7622079ec70d7688879d2a6
osmar570 <57583658+osmar570@users.noreply.github.com>
2023-03-16 14:00:36 -0300

    Update index.js
    alterando o modelo usado no chat de text-davinci-03 para o gpt-3.5-turbo

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const openai = new OpenAIApi(configuration);
 
 const getDavinciResponse = async (clientText) => {
     const options = {
-        model: "text-davinci-003", // Modelo GPT a ser usado
+        model: "gpt-3.5-turbo", // Modelo GPT a ser usado
         prompt: clientText, // Texto enviado pelo usuário
         temperature: 1, // Nível de variação das respostas geradas, 1 é o máximo
         max_tokens: 4000 // Quantidade de tokens (palavras) a serem retornadas pelo bot, 4000 é o máximo
